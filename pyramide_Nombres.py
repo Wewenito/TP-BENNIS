dc1b0e6e37e83716887efed39b33dc60735ef6c1
Wewenito <111244969+Wewenito@users.noreply.github.com>
2023-01-21 08:37:23 +0100

    Delete pyramide_Nombres.py

--- pyramide_Nombres.py
+++
@@ -1,6 +0,0 @@
-height = int(input("Entrez la hauteur de la pyramide : "))
-for i in range(1, height+1):
-    print(" " * (height-i), end='')
-    for j in range(1,i+1):
-        print(j, end=' ')
-    print()
